f49603aed00d546be2950de86f8e348db8e97f4f
Anna Lissa Saupan <annalissa.saupan@gmail.com>
2013-07-10 01:00:40 +0000

    Updated web.js. Added fs.readFileSync, buf.toString()

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+/*
+fs.readFileSync(filename, [options])#
+Synchronous version of fs.readFile. Returns the contents of the filename.
+
+If the encoding option is specified then this function returns a string. Otherwise it returns a buffer.
+*/
+
 var express = require('express');
+var fs = require('fs'); 
 
 var app = express.createServer(express.logger());
+var buf = fs.readFileSync('index.html'); 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
